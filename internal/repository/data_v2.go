8eff3153e004258021e561fcddb7e58f2b295778
pikachu0310 <pikachu13711@gmail.com>
2025-05-14 21:35:58 +0900

    Revert "✨ セーブデータは最新idを取得するように"
    This reverts commit 513c444d9147ecb941bf184bc1fdd18da383307e.

--- internal/repository/data_v2.go
+++ internal/repository/data_v2.go
@@ -82,14 +82,10 @@ func (r *Repository) GetLatestSave(ctx context.Context, userID string) (*domain.
 	var sd domain.SaveData
 	// 1) main row
 	err := r.db.GetContext(ctx, &sd, `
-SELECT
-  id
-FROM
-  save_data_v2
-WHERE
-  user_id = ?
-ORDER BY
-  created_at DESC
+SELECT * 
+FROM save_data_v2 
+WHERE user_id = ? 
+ORDER BY created_at DESC 
 LIMIT 1
 `, userID)
 	if err != nil {
